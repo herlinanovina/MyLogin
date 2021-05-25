0fb35fafbed80f50b9fdd8867805c514341e52d9
Herlina Novina <0320200011@polman.astra.ac.id>
2021-05-25 14:12:38 +0700

    penambahan fungsi excel

---
+++ MyLogin/MasterProduct.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyLogin
+{
+    public partial class MasterProduct : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-VHM5NEN\SQLEXPRESS;Initial Catalog=MyPractice;Integrated Security=True;");
+        public MasterProduct()
+        {
+            InitializeComponent();
+        }
+        DataClasses1DataContext db = new DataClasses1DataContext();
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtProductID.Text);
+            string item = txtItem.Text;
+            string design = txtDesign.Text;
+            string color = cbColor.Text;
+            DateTime expiredDate = DateTime.Parse(dateExpired.Text);
+            var data = new TB_M_PRODUCT
+            {
+                ID = id,
+                itemName = item,
+                color = color,
+                design = design,
+                expiredDate = expiredDate
+            };
+            db.TB_M_PRODUCTs.InsertOnSubmit(data);
+            db.SubmitChanges();
+            MessageBox.Show("Save Successfully");
+            txtDesign.Clear();
+            txtItem.Clear();
+            cbColor.Items.Clear();
+            LoadData();
+        }
+
+        void LoadData()
+        {
+            var st = from tb in db.TB_M_PRODUCTs select tb;
+            dt.DataSource = st;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            var st = from s in db.TB_M_PRODUCTs where s.itemName == txtSearchItem.Text || s.design == txtSearchDesign.Text select s;
+            dt.DataSource = st;
+        }
+
+        private void MasterProduct_Load_1(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlDataAdapter sda = new SqlDataAdapter("select isnull(max (cast (ID as int)),0) +1 from TB_M_PRODUCT", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            txtProductID.Text = dt.Rows[0][0].ToString();
+            LoadData();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            var delete = from s in db.TB_M_PRODUCTs where s.itemName == txtSearchItem.Text select s;
+            foreach (var t in delete)
+            {
+                db.TB_M_PRODUCTs.DeleteOnSubmit(t);
+            }
+            db.SubmitChanges();
+            MessageBox.Show("Delete Succesfully");
+            txtSearchDesign.Clear();
+            txtSearchItem.Clear();
+            LoadData();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string item = txtSearchItem.Text;
+            string design = txtSearchDesign.Text;
+            string color = cbColor.Text;
+            DateTime expiredDate = DateTime.Parse(dateExpired.Text);
+
+            var st = (from s in db.TB_M_PRODUCTs where s.ID == int.Parse(txtProductID.Text) select s).First();
+
+            st.itemName = item;
+            st.color = color;
+            st.design = design;
+            st.expiredDate = expiredDate;
+            db.SubmitChanges();
+
+            MessageBox.Show("Update Succesfuly");
+            txtDesign.Clear();
+            txtItem.Clear();
+            cbColor.SelectedIndex = -1;
+        }
+    }
+}
---
+++ MyLogin/masterproduct.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace MyLogin
+{
+    partial class MasterProduct
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSearchItem = new System.Windows.Forms.TextBox();
+            this.txtSearchDesign = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtProductID = new System.Windows.Forms.TextBox();
+            this.txtItem = new System.Windows.Forms.TextBox();
+            this.txtDesign = new System.Windows.Forms.TextBox();
+            this.cbColor = new System.Windows.Forms.ComboBox();
+            this.dateExpired = new System.Windows.Forms.DateTimePicker();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dt = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dt)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(179, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MASTER PRODUCT";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Item Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Design";
+            // 
+            // txtSearchItem
+            // 
+            this.txtSearchItem.Location = new System.Drawing.Point(155, 58);
+            this.txtSearchItem.Name = "txtSearchItem";
+            this.txtSearchItem.Size = new System.Drawing.Size(100, 22);
+            this.txtSearchItem.TabIndex = 3;
+            // 
+            // txtSearchDesign
+            // 
+            this.txtSearchDesign.Location = new System.Drawing.Point(155, 103);
+            this.txtSearchDesign.Name = "txtSearchDesign";
+            this.txtSearchDesign.Size = new System.Drawing.Size(100, 22);
+            this.txtSearchDesign.TabIndex = 4;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(352, 56);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 5;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Product ID";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 190);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Item Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(28, 221);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 17);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Design";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(28, 254);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 17);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Color";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(28, 285);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(89, 17);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Expired Date";
+            // 
+            // txtProductID
+            // 
+            this.txtProductID.Location = new System.Drawing.Point(182, 155);
+            this.txtProductID.Name = "txtProductID";
+            this.txtProductID.Size = new System.Drawing.Size(100, 22);
+            this.txtProductID.TabIndex = 11;
+            // 
+            // txtItem
+            // 
+            this.txtItem.Location = new System.Drawing.Point(182, 185);
+            this.txtItem.Name = "txtItem";
+            this.txtItem.Size = new System.Drawing.Size(100, 22);
+            this.txtItem.TabIndex = 12;
+            // 
+            // txtDesign
+            // 
+            this.txtDesign.Location = new System.Drawing.Point(182, 218);
+            this.txtDesign.Name = "txtDesign";
+            this.txtDesign.Size = new System.Drawing.Size(100, 22);
+            this.txtDesign.TabIndex = 13;
+            // 
+            // cbColor
+            // 
+            this.cbColor.FormattingEnabled = true;
+            this.cbColor.Items.AddRange(new object[] {
+            "merah",
+            "biru",
+            "hitam",
+            "abu"});
+            this.cbColor.Location = new System.Drawing.Point(182, 247);
+            this.cbColor.Name = "cbColor";
+            this.cbColor.Size = new System.Drawing.Size(121, 24);
+            this.cbColor.TabIndex = 14;
+            // 
+            // dateExpired
+            // 
+            this.dateExpired.Location = new System.Drawing.Point(182, 285);
+            this.dateExpired.Name = "dateExpired";
+            this.dateExpired.Size = new System.Drawing.Size(200, 22);
+            this.dateExpired.TabIndex = 15;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(133, 340);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(61, 23);
+            this.btnSave.TabIndex = 16;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(212, 340);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(66, 23);
+            this.btnUpdate.TabIndex = 17;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(296, 340);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(66, 23);
+            this.btnDelete.TabIndex = 18;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dt
+            // 
+            this.dt.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dt.Location = new System.Drawing.Point(53, 385);
+            this.dt.Name = "dt";
+            this.dt.RowTemplate.Height = 24;
+            this.dt.Size = new System.Drawing.Size(389, 150);
+            this.dt.TabIndex = 19;
+            // 
+            // MasterProduct
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 547);
+            this.Controls.Add(this.dt);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dateExpired);
+            this.Controls.Add(this.cbColor);
+            this.Controls.Add(this.txtDesign);
+            this.Controls.Add(this.txtItem);
+            this.Controls.Add(this.txtProductID);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearchDesign);
+            this.Controls.Add(this.txtSearchItem);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "MasterProduct";
+            this.Text = "masterproduct";
+            this.Load += new System.EventHandler(this.MasterProduct_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dt)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSearchItem;
+        private System.Windows.Forms.TextBox txtSearchDesign;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtProductID;
+        private System.Windows.Forms.TextBox txtItem;
+        private System.Windows.Forms.TextBox txtDesign;
+        private System.Windows.Forms.ComboBox cbColor;
+        private System.Windows.Forms.DateTimePicker dateExpired;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dt;
+    }
+}
